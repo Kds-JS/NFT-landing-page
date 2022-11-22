12782e3b0acdfaa2f3566e98cb2f224e9e697507
kds <kdskoudous98@gmail.com>
2022-11-22 12:35:14 +0100

    update animation

--- src/Components/Roadmap.js
+++ src/Components/Roadmap.js
@@ -1,12 +1,125 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
+import { motion} from "framer-motion";
+
 
 const Roadmap = () => {
     return (
-        <Box px={{base: "5%", md:"7%", lg: "10%"}} py='40px'>
+        <Box px={{base: "5%", md:"7%", lg: "10%"}} py={{base: '70px', md:'150px'}}>
             <Heading color="white" mb="60px" textAlign="center">Roadmap</Heading>
 
-            <Box>description</Box>
+            <Box position="relative">
+
+                <Box h="100%" position="absolute" left={{base: '10px', md:'50%'}} top="25px" w="4px" bg="white"></Box>
+
+                <Flex justify="flex-start" position="relative" as={motion.div}    
+                 initial={{
+                    opacity: 0,
+                    translateX: -100,
+                  }}
+                  whileInView={{ opacity: 1, translateX: 0, translateY: 0}}
+                  transition={{ duration: 5, delay:  2}} 
+                >
+
+                    <Center boxSize="40px" bg="primary.500" borderRadius="full" position="absolute" top="20px" left={{base: "-6px", md:"47.5%", lg:"47.8%", xl: "48.5%"}} border="4px" borderColor="white" ></Center>
+                    <Box w={{base: '100%', md: "45%"}} bg="#151515" borderRadius="20px" boxShadow="dark-lg" py="25px" px="20px" ml={{base: "45px", md:"0"}} mb={{base: "25px", md:"0"}}>
+                        <Flex justify="space-between" align="center">
+                            <Text fontSize="20px" fontWeight="bold" color="white">DAO & Reward System</Text>
+                            <Button bg="white" color="black" _hover={{bg: "white"}}>Q4 2022</Button>
+                        </Flex>
+                        <Text mt="25px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure rerum recusandae perspiciatis saepe molestias! Facere accusamus veniam quas officia aliquid similique, ducimus debitis id eum saepe nemo fugiat nam animi.</Text>
+                    </Box>
+                </Flex>
+
+                <Flex justify="flex-end" position="relative" as={motion.div}    
+                 initial={{
+                    opacity: 0,
+                    translateX: -100,
+                  }}
+                  whileInView={{ opacity: 1, translateX: 0, translateY: 0}}
+                  transition={{ duration: 5, delay:  2}} >
+                    <Center boxSize="40px" bg="primary.500" borderRadius="full" position="absolute" top="20px" left={{base: "-6px", md:"47.5%", lg:"47.8%", xl: "48.5%"}} border="4px" borderColor="white" ></Center>
+                    <Box w={{base: '100%', md: "45%"}} bg="#151515" borderRadius="20px" boxShadow="dark-lg" py="25px" px="20px"  ml={{base: "45px", md:"0"}} mb={{base: "25px", md:"0"}}>
+                        
+                        <Flex justify="space-between" align="center">
+                            <Text fontSize="20px" fontWeight="bold" color="white">NFT Launchpad</Text>
+                            <Button bg="white" color="black" _hover={{bg: "white"}}>Q1 2023</Button>
+                        </Flex>
+                        <Text mt="25px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure rerum recusandae perspiciatis saepe molestias!.</Text>
+                    </Box>
+                </Flex>
+
+                <Flex justify="flex-start" position="relative" as={motion.div}    
+                 initial={{
+                    opacity: 0,
+                    translateX: -100,
+                  }}
+                  whileInView={{ opacity: 1, translateX: 0, translateY: 0}}
+                  transition={{ duration: 5, delay:  2}}>
+                    <Center boxSize="40px" bg="primary.500" borderRadius="full" position="absolute" top="20px" left={{base: "-6px", md:"47.5%", lg:"47.8%", xl: "48.5%"}} border="4px" borderColor="white" ></Center>
+                    <Box w={{base: '100%', md: "45%"}} bg="#151515" borderRadius="20px" boxShadow="dark-lg" py="25px" px="20px" ml={{base: "45px", md:"0"}} mb={{base: "25px", md:"0"}}>
+                        <Flex justify="space-between" align="center">
+                            <Text fontSize="20px" fontWeight="bold" color="white">Alpha Lions Club Shop</Text>
+                            <Button bg="white" color="black" _hover={{bg: "white"}}>Q2 2023</Button>
+                        </Flex>
+                        <Text mt="25px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolore. Ex est placeat eius mollitia voluptatibus ducimus eveniet sint sapiente, voluptates!</Text>
+                    </Box>
+                </Flex>
+
+                <Flex justify="flex-end" position="relative" as={motion.div}    
+                 initial={{
+                    opacity: 0,
+                    translateX: -100,
+                  }}
+                  whileInView={{ opacity: 1, translateX: 0, translateY: 0}}
+                  transition={{ duration: 5, delay:  2}}>
+                    <Center boxSize="40px" bg="primary.500" borderRadius="full" position="absolute" top="20px" left={{base: "-6px", md:"47.5%", lg:"47.8%", xl: "48.5%"}} border="4px" borderColor="white" ></Center>
+                    <Box w={{base: '100%', md: "45%"}} bg="#151515" borderRadius="20px" boxShadow="dark-lg" py="25px" px="20px"  ml={{base: "45px", md:"0"}} mb={{base: "25px", md:"0"}}>
+                        
+                        <Flex justify="space-between" align="center">
+                            <Text fontSize="20px" fontWeight="bold" color="white">3D & Augmented Reality NFT Artwork</Text>
+                            <Button bg="white" color="black" _hover={{bg: "white"}}>Q3 2023</Button>
+                        </Flex>
+                        <Text mt="25px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, hic cupiditate. Veritatis minus, dolorum veniam quos quia perspiciatis, excepturi maxime dicta, laudantium error adipisci! Quaerat, delectus! Aut temporibus porro rerum?</Text>
+                    </Box>
+                </Flex>
+
+                <Flex justify="flex-start" position="relative" as={motion.div}    
+                 initial={{
+                    opacity: 0,
+                    translateX: -100,
+                  }}
+                  whileInView={{ opacity: 1, translateX: 0, translateY: 0}}
+                  transition={{ duration: 5, delay:  2}}>
+                    <Center boxSize="40px" bg="primary.500" borderRadius="full" position="absolute" top="20px" left={{base: "-6px", md:"47.5%", lg:"47.8%", xl: "48.5%"}} border="4px" borderColor="white" ></Center>
+                    <Box w={{base: '100%', md: "45%"}} bg="#151515" borderRadius="20px" boxShadow="dark-lg" py="25px" px="20px" ml={{base: "45px", md:"0"}} mb={{base: "25px", md:"0"}}>
+                        <Flex justify="space-between" align="center">
+                            <Text fontSize="20px" fontWeight="bold" color="white">Totally Orbital</Text>
+                            <Button bg="white" color="black" _hover={{bg: "white"}}>Q4 2023</Button>
+                        </Flex>
+                        <Text mt="25px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure rerum recusandae perspiciatis saepe molestias! Facere accusamus veniam quas officia aliquid similique, ducimus debitis id eum saepe nemo fugiat nam animi.</Text>
+                    </Box>
+                </Flex>
+
+                <Flex justify="flex-end" position="relative" as={motion.div}    
+                 initial={{
+                    opacity: 0,
+                    translateX: -100,
+                  }}
+                  whileInView={{ opacity: 1, translateX: 0, translateY: 0}}
+                  transition={{ duration: 5, delay:  2}}>
+                    <Center boxSize="40px" bg="primary.500" borderRadius="full" position="absolute" top="20px" left={{base: "-6px", md:"47.5%", lg:"47.8%", xl: "48.5%"}} border="4px" borderColor="white" ></Center>
+                    <Box w={{base: '100%', md: "45%"}} bg="#151515" borderRadius="20px" boxShadow="dark-lg" py="25px" px="20px"  ml={{base: "45px", md:"0"}} mb={{base: "25px", md:"0"}}>
+                        
+                        <Flex justify="space-between" align="center">
+                            <Text fontSize="20px" fontWeight="bold" color="white">What's next?</Text>
+                            <Button bg="white" color="black" _hover={{bg: "white"}}>After</Button>
+                        </Flex>
+                        <Text mt="25px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, dolorum voluptatum autem nulla officia dolorem odio!</Text>
+                    </Box>
+                </Flex>
+                
+            </Box>
         </Box>
     );
 };
--- src/Components/Team.js
+++ src/Components/Team.js
@@ -6,69 +6,68 @@ import img2 from '../images/326.png';
 import img3 from '../images/105.png';
 import img4 from '../images/151.png';
 
+import { motion } from 'framer-motion';
+
 const Team = () => {
+
+    const team = [
+        {
+            name: 'KDS',
+            img: img1,
+            job: 'Smart Contract Developer'
+        },
+        {
+            name: 'JULIAN',
+            img: img2,
+            job: 'Frontend Developer'
+        },
+        {
+            name: 'KARL',
+            img: img3,
+            job: '2D & 3D Designer'
+        },
+        {
+            name: 'KYLIAN',
+            img: img4,
+            job: 'Business Developer'
+        }
+    ]
+
+
     return (
         <Box px={{base: "5%", md:"7%", lg: "10%"}} py='40px' bgGradient='linear(to-tr, primary.900, primary.500)'>
             <Heading color="white" mb="60px" textAlign="center">Team</Heading>
 
             <Grid templateColumns={{base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)', lg: 'repeat(4, 1fr)'}} gap="30px">
 
-                <Box boxShadow="md" h='350px' bgGradient='linear(to-r, secondary.800, primary.900)' border="2px" borderColor="white" borderRadius="25px">
-                    <Image src={img1} h="75%" w="100%" objectFit="cover" px="15px" bgImage={bgTeam} borderRadius="25px 25px 0 0"/>
+                {team.map((member, index) => (
+
+                <Box key={index} boxShadow="md" h='350px' bgGradient='linear(to-r, secondary.800, primary.900)' border="2px" borderColor="white" borderRadius="25px" as={motion.div}
+                initial={{ opacity: 0, y: 20}}
+                    whileInView={{ opacity: 1, y:0}}
+                    transition={{
+                        duration: 0.5,
+                        delay: 0.2 * (index + 1)
+                    }}
+                >
+                    <Box h="75%" overflow="hidden" borderRadius="25px 25px 0 0">
+                    <Image src={member.img} w="100%" h="100%" objectFit="cover" px="15px" bgImage={bgTeam} borderRadius="25px 25px 0 0" transform="scale(1)"
+                    transition= "0.3s ease-in-out"
+                    _hover={{transform: "scale(1.1)"}}/>
+                    </Box>
 
                     <Box py="10px" h='25%' color="white" textAlign="center" fontSize="20px" >
                         <Text fontWeight="bold">
-                         KDS
+                            {member.name}
                         </Text>
 
                         <Text>
-                        Smart Contract Developer
+                            {member.job}
                         </Text>
                     </Box>
                 </Box>
 
-                <Box boxShadow="md" h='350px' bgGradient='linear(to-r, secondary.800, primary.900)' border="2px" borderColor="white" borderRadius="25px">
-                    <Image src={img2} h="75%" w="100%" objectFit="cover" px="15px" bgImage={bgTeam} borderRadius="25px 25px 0 0"/>
-
-                    <Box py="10px" h='25%' color="white" textAlign="center" fontSize="20px" >
-                        <Text fontWeight="bold">
-                         JULIAN
-                        </Text>
-
-                        <Text>
-                        web3 Developer
-                        </Text>
-                    </Box>
-                </Box>
-
-                <Box boxShadow="md" h='350px' bgGradient='linear(to-r, secondary.800, primary.900)' border="2px" borderColor="white" borderRadius="25px">
-                    <Image src={img3} h="75%" w="100%" objectFit="cover" px="15px" bgImage={bgTeam} borderRadius="25px 25px 0 0"/>
-
-                    <Box py="10px" h='25%' color="white" textAlign="center" fontSize="20px" >
-                        <Text fontWeight="bold">
-                         KARL
-                        </Text>
-
-                        <Text>
-                        2D & 3D Designer
-                        </Text>
-                    </Box>
-                </Box>
-
-                <Box boxShadow="md" h='350px' bgGradient='linear(to-r, secondary.800, primary.900)' border="2px" borderColor="white" borderRadius="25px">
-                    <Image src={img4} h="75%" w="100%" objectFit="cover" px="15px" bgImage={bgTeam} borderRadius="25px 25px 0 0"/>
-
-                    <Box py="10px" h='25%' color="white" textAlign="center" fontSize="20px" >
-                        <Text fontWeight="bold">
-                         KYLIAN
-                        </Text>
-
-                        <Text>
-                        Business Developer
-                        </Text>
-                    </Box>
-                </Box>
-
+                ))}
 
             </Grid>
         </Box>
